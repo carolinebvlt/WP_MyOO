fff297ee6c757b13c26b2a43f9f54e7727abc8d6
Caroline Bieuvelet <caroline.bieuvelet@gmail.com>
2018-03-22 21:33:55 +0100

    add page 'init'

--- MyOO.php
+++ MyOO.php
@@ -10,6 +10,69 @@ class MyOO
   public function __construct(){
     include_once plugin_dir_path( __FILE__ ).'/MyOO_Menu.php';
     new MyOO_Menu();
+    add_action('init', [$this, 'ajout_page']);
   }
+  public function ajout_page(){
+
+    $content = "mon texte";
+    $postTitle = 'BLOP';
+
+    $args = [
+      'post_status'=> 'publish',
+      'post_type'=> 'page',
+      'post_title'=> $postTitle
+    ];
+    $query = new WP_Query( $args );
+
+    if($query->post->post_title !== $postTitle){
+        $page = [
+            'post_title'   => $postTitle,
+            'post_content' => $content,
+            'post_status'  => 'publish',
+            'post_author'  => 1,
+            'post_type'    => 'page',
+            'post_parent'  => 0
+        ];
+        $insert_id = wp_insert_post( $page );
+    }
+
+    // if(!$query->post){
+    //   $page = [
+    //       'post_title'   => $postTitle,
+    //       'post_content' => $content,
+    //       'post_status'  => 'publish',
+    //       'post_author'  => 1,
+    //       'post_type'    => 'page',
+    //       'post_parent'  => 0
+    //   ];
+    //   $insert_id = wp_insert_post( $page );
+    }
+    // global $wpdb;
+
+    // $query = $wpdb->prepare(
+    //     'SELECT ID FROM ' . $wpdb->posts . '
+    //         WHERE post_title = %s
+    //         AND post_type = \'page\'
+    //         AND post_status = \'publish\'',
+    //     $postTitle
+    // );
+    // $wpdb->query( $query );
+
+
+    // if ( $wpdb->num_rows ) {
+    //     // Title already exists
+    // }
+    // else {
+    //   $page = [
+    //       'post_title'   => $postTitle,
+    //       'post_content' => $content,
+    //       'post_status'  => 'publish',
+    //       'post_author'  => 1,
+    //       'post_type'    => 'page',
+    //       'post_parent'  => 0
+    //   ];
+    //   $insert_id = wp_insert_post( $page );
+    // }
+  // }
 }
 new MyOO();
