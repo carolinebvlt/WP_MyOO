aecdf23cf349124adb8e7297d0d11812f5e9840f
Caroline Bieuvelet <caroline.bieuvelet@gmail.com>
2018-03-24 14:19:45 +0100

    del page menu

--- MyOO_Page_Builder.php
+++ MyOO_Page_Builder.php
@@ -5,35 +5,7 @@ class MyOO_Page_Builder
     add_action('init', [$this, 'ajout_page_menu']);
   }
 
-/****************  PAGE MENU  *****************/
-  public function ajout_page_menu(){
 
-    $_tartines = ['a', 'b', 'c', 'd', 'e']; /*AM*/
-    $content = "<h1><u><i>Carte des tartines</i></u></h1>";
-    foreach ($_tartines as $tartine) {
-      $content = $content.'<p>'.$tartine.'</p>';
-    }
-    $postTitle = 'Menu';
-
-    $args = [
-      'post_status'=> 'publish',
-      'post_type'=> 'page',
-      'post_title'=> $postTitle
-    ];
-    $query = new WP_Query( $args );
-
-    if($query->post->post_title !== $postTitle){
-        $page = [
-            'post_title'   => $postTitle,
-            'post_content' => $content,
-            'post_status'  => 'publish',
-            'post_author'  => 1,
-            'post_type'    => 'page',
-            'post_parent'  => 0
-        ];
-        $insert_id = wp_insert_post( $page );
-    }
-  }
 
 /****************  PAGE ORDER  *****************/
 
