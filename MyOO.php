55d3d83dc023d52d84bcfb4ee29ec853c44c1287
Caroline Bieuvelet <caroline.bieuvelet@gmail.com>
2018-03-24 14:17:55 +0100

    page builder

--- MyOO.php
+++ MyOO.php
@@ -10,35 +10,8 @@ class MyOO
   public function __construct(){
     include_once plugin_dir_path( __FILE__ ).'/MyOO_Menu.php';
     new MyOO_Menu();
-    add_action('init', [$this, 'ajout_page']);
-  }
-  public function ajout_page(){
-
-    $_tartines = ['a', 'b', 'c', 'd'];
-    $content = "<h1>Test</h1>";
-    foreach ($_tartines as $tartine) {
-      $content = $content.'<p>'.$tartine.'</p>';
-    }
-    $postTitle = 'BLOP';
-
-    $args = [
-      'post_status'=> 'publish',
-      'post_type'=> 'page',
-      'post_title'=> $postTitle
-    ];
-    $query = new WP_Query( $args );
-
-    if($query->post->post_title !== $postTitle){
-        $page = [
-            'post_title'   => $postTitle,
-            'post_content' => $content,
-            'post_status'  => 'publish',
-            'post_author'  => 1,
-            'post_type'    => 'page',
-            'post_parent'  => 0
-        ];
-        $insert_id = wp_insert_post( $page );
-    }
+    include_once plugin_dir_path( __FILE__ ).'/MyOO_Page_Builder.php';
+    new MyOO_Page_Builder();
   }
 }
 new MyOO();
