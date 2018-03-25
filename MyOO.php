23b4563921f2cba76bce256e42218e94e7966b46
Caroline Bieuvelet <caroline.bieuvelet@gmail.com>
2018-03-25 15:12:52 +0200

    cleaning

--- MyOO.php
+++ MyOO.php
@@ -1,20 +1,20 @@
 <?php
 /*
 Plugin Name: MyOO
-Description: Orders organizer Plugin
+Description: Orders organizer pour Tartinette
 Version: 0.1
 Author: CeaB
 */
 class MyOO
 {
   public function __construct(){
-    include_once plugin_dir_path( __FILE__ ).'/MyOO_CPT_Tartines.php';
+    include_once plugin_dir_path( __FILE__ ).'/Orders_Organizer/MyOO_CPT_Tartines.php';
     new MyOO_CPT_Tartines();
-    include_once plugin_dir_path( __FILE__ ).'/MyOO_Orders_Organizer.php';
+    include_once plugin_dir_path( __FILE__ ).'/Orders_Organizer/MyOO_Orders_Organizer.php';
     new MyOO_Orders_Organizer();
-    include_once plugin_dir_path( __FILE__ ).'/MyOO_Registration_Module.php';
+    include_once plugin_dir_path( __FILE__ ).'/Users_Organizer/MyOO_Registration_Module.php';
     new MyOO_Registration_Module();
-    include_once plugin_dir_path( __FILE__ ).'/MyOO_Account_Module.php';
+    include_once plugin_dir_path( __FILE__ ).'/Users_Organizer/MyOO_Account_Module.php';
     new MyOO_Account_Module();
   }
 }
