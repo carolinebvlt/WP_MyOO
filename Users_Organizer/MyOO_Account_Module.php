30dce073fa5a004b8517ba2ec283f5ce05afee6b
Caroline Bieuvelet <caroline.bieuvelet@gmail.com>
2018-03-27 21:33:02 +0200

    my_forms_script

--- Users_Organizer/MyOO_Account_Module.php
+++ Users_Organizer/MyOO_Account_Module.php
@@ -9,6 +9,7 @@ class MyOO_Account_Module
   public function __construct(){
     add_action('init', [$this, 'add_account_page']);
     add_action('wp_loaded', [$this, 'connexion']);
+    add_action('wp', [$this, 'enqueue_my_script']);
   }
 
   public function add_account_page(){
@@ -32,14 +33,6 @@ class MyOO_Account_Module
     }
   }
 
-  public function connexion_form(){
-      return "<form method='post' action=''>
-                <input type='text' name='email' placeholder='Votre email'/>
-                <input type='password' name='password' placeholder='Mot de passe'/>
-                <input type='submit' name='submit_connexion' value='Se connecter'/>
-              </form>";
-  }
-
   public function connexion(){
     if(isset($_POST['submit_connexion'])){
       $email = $_POST['email'];
@@ -56,25 +49,38 @@ class MyOO_Account_Module
       $sql = "SELECT * FROM {$wpdb->prefix}posts WHERE post_title = 'Mon compte' AND post_status = 'publish' AND post_type = 'page' ";
       $row = $wpdb->get_row($sql);
       $id_page = $row->ID;
-      $z = $this->my_account_html();
+      $html = $this->my_account_html();
       wp_update_post([
         'ID' => $id_page,
-        'post_content' => $z
+        'post_content' => $html
       ]);
     }
+  }
+
+  public function connexion_form(){
+      return "<form method='post' action=''>
+                <input type='text' name='email' placeholder='Votre email'/>
+                <input type='password' name='password' placeholder='Mot de passe'/>
+                <input type='submit' name='submit_connexion' value='Se connecter'/>
+              </form>";
   }
 
   public function my_account_html(){
     return "<div>
               <span>Tribu ".$_SESSION['user_data']->tribu."</span>
               <form method='post' action=''>
-                <input type='submit' name='child1' value='Child 1'/>
-                <input type='submit' name='child2' value='Child 2'/>
+                <input type='button' name='child1' onclick='hop()' value='Child one'/>
+                <input type='button' name='child2' onclick='hophop()' value='Child two'/>
                 <input type='submit' name='add_child' value='Add'/>
               </form>
             </div>
             <div id='my_forms'></div>
-            <h1>Ma commande</h1>";
+            <h1>Ma commande </h1>";
+  }
+
+  public function enqueue_my_script(){
+    wp_register_script('my_forms_script', plugin_dir_url(__FILE__) . '../assets/my_forms_script.js');
+    wp_enqueue_script('my_forms_script');
   }
 
 } // end class
