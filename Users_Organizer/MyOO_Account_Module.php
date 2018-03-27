9f48f8aa46427f7eb96a4734bcdc4ffdbae81448
Caroline Bieuvelet <caroline.bieuvelet@gmail.com>
2018-03-27 14:10:04 +0200

    backup

--- Users_Organizer/MyOO_Account_Module.php
+++ Users_Organizer/MyOO_Account_Module.php
@@ -9,6 +9,7 @@ class MyOO_Account_Module
 {
   public function __construct(){
     add_action('init', [$this, 'add_account_page']);
+    add_action('wp_loaded', [$this, 'connexion']);
   }
 
   public function add_account_page(){
@@ -42,6 +43,21 @@ class MyOO_Account_Module
                 <input type='password' name='password' placeholder='Mot de passe'/>
                 <input type='submit' name='submit_connexion' value='Se connecter'/>
               </form>";
+    }
+  }
+
+  public function connexion(){
+    if(isset($_POST['submit_connexion'])){
+      $email = $_POST['email'];
+      $password = $_POST['password'];
+      global $wpdb;
+      $data = $wpdb->get_row( "SELECT * FROM {$wpdb->prefix}tartinette_users WHERE email = '$email' ");
+      if($data->pass_h === sha1($password)){
+        echo "Welcome ".$data->first_name;
+      }
+      else{
+        echo "Mauvais mot de passe !";
+      }
     }
   }
 
