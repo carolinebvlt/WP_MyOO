bf7654176bbc9883a42cf25c856e5d8f3a2d06be
Caroline Bieuvelet <caroline.bieuvelet@gmail.com>
2018-03-27 20:56:31 +0200

    connexion ok

--- Users_Organizer/MyOO_Account_Module.php
+++ Users_Organizer/MyOO_Account_Module.php
@@ -2,7 +2,6 @@
 /*
   - Création d'une page "My account"
   - Connexion form
-  - Connexion : $_SESSION['connected'] === true
 */
 
 class MyOO_Account_Module
@@ -13,7 +12,7 @@ class MyOO_Account_Module
   }
 
   public function add_account_page(){
-    $content = $this->account_page();
+    $content = $this->connexion_form();
     $postTitle = 'Mon compte';
 
     global $wpdb;
@@ -29,21 +28,16 @@ class MyOO_Account_Module
             'post_type'    => 'page',
             'post_parent'  => 0
         ];
-        $insert_id = wp_insert_post( $page );
+        wp_insert_post($page);
     }
   }
 
-  public function account_page(){
-    if(isset($_SESSION['connected'])&&$_SESSION['connected']===true){
-      return 'Hello you !';
-    }
-    else{
+  public function connexion_form(){
       return "<form method='post' action=''>
                 <input type='text' name='email' placeholder='Votre email'/>
                 <input type='password' name='password' placeholder='Mot de passe'/>
                 <input type='submit' name='submit_connexion' value='Se connecter'/>
               </form>";
-    }
   }
 
   public function connexion(){
@@ -53,13 +47,34 @@ class MyOO_Account_Module
       global $wpdb;
       $data = $wpdb->get_row( "SELECT * FROM {$wpdb->prefix}tartinette_users WHERE email = '$email' ");
       if($data->pass_h === sha1($password)){
-        echo "Welcome ".$data->first_name;
+        $_SESSION['connected'] = true;
+        $_SESSION['user_data'] = $data;
       }
-      else{
-        echo "Mauvais mot de passe !";
-      }
+    }
+    if($_SESSION['connected'] === true){
+      global $wpdb;
+      $sql = "SELECT * FROM {$wpdb->prefix}posts WHERE post_title = 'Mon compte' AND post_status = 'publish' AND post_type = 'page' ";
+      $row = $wpdb->get_row($sql);
+      $id_page = $row->ID;
+      $z = $this->my_account_html();
+      wp_update_post([
+        'ID' => $id_page,
+        'post_content' => $z
+      ]);
     }
   }
 
+  public function my_account_html(){
+    return "<div>
+              <span>Tribu ".$_SESSION['user_data']->tribu."</span>
+              <form method='post' action=''>
+                <input type='submit' name='child1' value='Child 1'/>
+                <input type='submit' name='child2' value='Child 2'/>
+                <input type='submit' name='add_child' value='Add'/>
+              </form>
+            </div>
+            <div id='my_forms'></div>
+            <h1>Ma commande</h1>";
+  }
 
 } // end class
